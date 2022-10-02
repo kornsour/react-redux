28f7294e90fa832bd3e55244f4be699f9fab7791
kornsour <ajkaiserauer@gmail.com>
2022-10-02 17:04:45 -0400

    custom hook

--- 7-videos-refactor-project/src/components/App.js
+++ 7-videos-refactor-project/src/components/App.js
@@ -1,33 +1,21 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "../api/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideo";
 
 // refactoring class component to function component
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("buildings");
 
-  // Empty array as second argument means only run this one time
   useEffect(() => {
-    onTermSubmit("buildings");
-  }, []);
-
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
-
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         {/* If we don't put a row inside the grid, 
                   the video detail and list may try to load 
