d603c28b616739637c7e148e552ba5afaad9e604
Andrew Kaiserauer <ajkaiserauer@gmail.com>
2023-01-16 09:25:29 -0500

    new project

--- 10-streams-project/client/src/index.js
+++ 10-streams-project/client/src/index.js
@@ -4,12 +4,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 const container = document.querySelector("#root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
